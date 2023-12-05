c946937451126880bc77ec306f21cb8210b74dc9
Shashwat Murawala <111159071+shashwatmurawala@users.noreply.github.com>
2023-12-05 05:30:20 -0500

    bishop completed, need to check if compiles

--- bishop.cc
+++ bishop.cc
@@ -25,54 +25,70 @@ bool Bishop::canMove(const std::string &start,const std::string &end, Piece ** b
 	pair<int, int> begin = getPos(start);
 	pair<int, int> fin = getPos(end);
 
-
+	pair<int, int> coord1;
+	pair<int, int> coord2;
+	coord1.first = 7;
+	coord1.second = 0;
+	coord2.first = 0;
+	coord2.second = 7;
 
 	// moving down on the right diagonal
-	if (!(begin==0) && !(begin == 63) && (begin % 7 == fin % 7) && begin < fin) {
+	if (!(begin==coord1) && !(begin == coord2) && 
+		(begin.first - fin.first == begin.second - fin.second) && 
+		begin.first < fin.first &&
+		begin.second < fin.second) {
 		while (true) {
-			begin += 7;
-			if (begin == fin && (b[begin]->isEmpty() || (isWhite() != b[begin]->isWhite()))) {
+			++begin.first;
+			++begin.second;
+			if (begin == fin && ((b[begin.first][begin.second].isEmpty()) || (isWhite() != b[begin.first][begin.second].isWhite()))) {
 				return true;
 			}
-			else if (begin == fin && b[begin]->isEmpty()) {
+			else if (begin == fin && b[begin.first][begin.second].isEmpty()) {
 				return true;
 			}
-			else if (!b[begin]->isEmpty()) {
+			else if (!b[begin.first][begin.second].isEmpty()) {
 				return false;
 			}
 		}
 	}
 
 	// moving up on the right diagonal
-	else if (!(begin==0) && !(begin == 63) && (begin % 7 == fin % 7) && begin > fin) {
+	else if (!(begin==coord1) && !(begin == coord2) && 
+		(begin.first - fin.first == fin.second - begin.second) && 
+		begin.first > fin.first &&
+		begin.second < fin.second) {
 		while (true) {
-			begin -= 7;
-			if (begin == fin && (b[begin]->isEmpty() || (isWhite() != b[begin]->isWhite()))) {
+			--begin.first;
+			++begin.second;
+			if (begin == fin && (b[begin.first][begin.second].isEmpty() || (isWhite() != b[begin.first][begin.second].isWhite()))) {
 				return true;
 			}
-			else if (begin == fin && b[begin]->isEmpty()) {
+			else if (begin == fin && b[begin.first][begin.second].isEmpty()) {
 				return true;
 			}
-			else if(!b[begin]->isEmpty()) {
+			else if(!b[begin.first][begin.second].isEmpty()) {
 				return false;
 			}
 		} 
 	}
 
 	// moving down on the left diagonal
-	else if (begin % 9 == fin % 9 && begin < fin) {
+	else if ((fin.first - begin.first == begin.second - fin.second) && 
+		begin.first < fin.first &&
+		begin.second > fin.second) {
 		if(onLeft(begin.second)){
 				return false;
 		}
 		while (true) {
-			begin += 9;
-			if (begin == fin && (b[begin]->isEmpty() || (isWhite() != b[begin]->isWhite()))) {
+			++begin.first;
+			--begin.second;
+			if (begin == fin && (b[begin.first][begin.second].isEmpty() || (isWhite() != b[begin.first][begin.second].isWhite()))) {
 				return true;
 			}
-			else if (begin == fin && b[begin]->isEmpty()) {
+			else if (begin == fin && b[begin.first][begin.second].isEmpty()) {
 				return true;
 			}
-			else if (!b[begin]->isEmpty()) {
+			else if (!b[begin.first][begin.second].isEmpty()) {
 				return false;
 			}
 			else if(onRight(begin.second)){
@@ -82,19 +98,22 @@ bool Bishop::canMove(const std::string &start,const std::string &end, Piece ** b
 	}
 
 	// moving up on the left diagonal
-	else if (begin % 9 == fin % 9 && begin > fin) {
+	else if ((fin.first - begin.first == fin.second - begin.second) && 
+		begin.first > fin.first &&
+		begin.second > fin.second) {
 		if (onRight(begin.second)){
 				return false;
 		}
 		while (true) {
-			begin -= 9;
-			if (begin == fin && (b[begin]->isEmpty() || (isWhite() != b[begin]->isWhite()))) {
+			--begin.first;
+			--begin.second;
+			if (begin == fin && (b[begin.first][begin.second].isEmpty() || (isWhite() != b[begin.first][begin.second].isWhite()))) {
 				return true;
 			}
-			else if (begin == fin && b[begin]->isEmpty()) {
+			else if (begin == fin && b[begin.first][begin.second].isEmpty()) {
 				return true;
 			}
-			else if (!b[begin]->isEmpty()) {
+			else if (!b[begin.first][begin.second].isEmpty()) {
 				return false;
 			}
 			else if(onLeft(begin.second)){
