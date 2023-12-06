d60034efeb587b59b0cb5dfd75fb0f81bb2c93da
Shashwat Murawala <111159071+shashwatmurawala@users.noreply.github.com>
2023-12-05 21:37:51 -0500

    works

--- knight.cc
+++ knight.cc
@@ -9,17 +9,19 @@ bool Knight::isBlank() const{
 }
 
 bool Knight::validMove(const std::string &start,const std::string &end, Piece ** b) const {
+	int org_x = arrayloc(start) % 8;
+	int org_y = arrayloc(start) / 8;
+	int newloc_x = arrayloc(end) % 8;
+	int newloc_y = arrayloc(end) / 8;
 	if((start[0] < 'a')||(start[0] > 'h')||(start[1] < '1')||(start[1] > '8')) return false;
 	if((end[0] < 'a')||(end[0] > 'h')||(end[1] < '1')||(end[1] > '8')) return false;
 	if(!b[arrayloc(end)]->isBlank() && isWhite() == b[arrayloc(end)]->isWhite()){
 		return false;
 	}
-	int org_x = arrayloc(start) % 8;
-	int org_y = arrayloc(start) / 8;
-	int newloc_x = arrayloc(end) % 8;
-	int newloc_y = arrayloc(end) / 8;
-	if ((abs(org_x - newloc_x) == 1 && abs(org_y - newloc_y) == 2 ) ||
-		(abs(org_x - newloc_x) == 2 && abs(org_y - newloc_y) == 1)) {
+	if (abs(org_x - newloc_x) == 1 && abs(org_y - newloc_y) == 2 ) {
+		return true;
+	}
+	else if (abs(org_x - newloc_x) == 2 && abs(org_y - newloc_y) == 1){
 		return true;
 	}
 	else {
